12af5605e82f7b764b6193a20756ac4111cafe2a
dfdavid <david.do.it@gmail.com>
2019-06-22 00:17:12 -0300

    se modifica la clase silo
    Silo ahora arroja excepciones en uno de sus constructores.

---
+++ Silo.java
@@ -0,0 +1,61 @@
+
+/**
+ * Write a description of class Silo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Silo extends Recipiente //throws Exception
+{
+    // instance variables - replace the example below with your own
+    //hereda:
+        //static int CAPACIDAD_MAXIMA_DEFAULT = 2000;
+    //int capacidadMaxima;
+    //int cantidadDisponible;
+    //campos definidos
+    private Ingrediente ingrediente;
+
+    /**
+     * constructor A: 2 parametros
+     * Constructor for objects of class Silo 
+     */
+    public Silo(Ingrediente ingQueTendra, int capacidadASetear) throws Exception
+    {
+        // initialise instance variables
+        if ( capacidadASetear> CAPACIDAD_MAXIMA_DEFAULT){
+            throw new CapacidadExcedidaException();            
+        }
+        
+        if (capacidadASetear<=0 ){
+            throw new Exception();
+        }
+        this.ingrediente=ingQueTendra;
+        this.capacidadMaxima=capacidadASetear;
+        this.cantidadDisponible=0;
+        
+    }
+
+    /**
+     * constructor B: 1 parametro
+     * Constructor for objects of class Silo 
+     */
+    public Silo(Ingrediente ingQueTendra)
+    {
+        // initialise instance variables
+        this.ingrediente=ingQueTendra;
+        this.capacidadMaxima=CAPACIDAD_MAXIMA_DEFAULT;
+        this.cantidadDisponible=0;
+    }
+    
+    /**
+     * devuelve tipo de ingrediente almacenado
+     * 
+     * @param  void
+     * @return Ingrediente 
+     */
+    public Ingrediente getTipoIngredienteAlmacenado()
+    {
+        // put your code here
+        return this.ingrediente;
+    }
+}
